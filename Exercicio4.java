fe178bdf45c7d72982fb31128a914ab78a447d96
Gustavo-Hanneman <gusttavo6661@gmail.com>
2021-07-02 13:51:44 -0300

    Subindo exercícios

---
+++ Exercicio4.java
@@ -0,0 +1,28 @@
+package Exercício;
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        int num, resposta;
+        
+        System.out.println("Digite um número: ");
+        num = entrada.nextInt();
+        resposta = num % 2;
+        
+        if(resposta == 0) {
+            System.out.println("Seu número é par:"); 
+            System.out.println(Math.sqrt(num));
+        }
+        else {
+            System.out.println("Seu número é ímpar: ");
+            System.out.println(Math.pow(num, 2));
+        }
+        
+
+    }
+
+}
